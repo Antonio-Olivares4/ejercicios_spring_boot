0c2bcb6b0074b1d713d573b4952e96219b2de45f
Antonio-Olivares4 <olivares.lopez.antonio-vladimir@iescamas.es>
2024-11-22 10:14:17 +0100

    Modificado el bucle para que empiece por uno en vez de cero.

--- estructuras/src/main/java/com/ejercicio/estructuras/Controlador.java
+++ estructuras/src/main/java/com/ejercicio/estructuras/Controlador.java
@@ -4,32 +4,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
+//Ejercicio 1.
 @RestController
 public class Controlador {
 	@PostMapping("/parImpar")
 	public String ejercicio1(@RequestParam int numero) {
-		String ret="";
-		if(numero%2==0) {
-			ret +="Es par el "  + numero;
-		}else {
-			ret="Hola mundo.";
-			
+		String ret = "";
+		if (numero % 2 == 0) {
+			ret += "Es par el " + numero;
+		} else {
+			ret = "Hola mundo.";
+
 		}
 		return ret;
 	}
-	
-	
+//Ejercicio 2.
 	@PostMapping("/tablaMultiplicar")
 	public String ejercicio2(@RequestParam int numero) {
-		String nMultiplicar="";
-		for(int i =0; i<=10; i++) {
-			nMultiplicar+="<h3>" + numero*i + "</h3>";
+		String nMultiplicar = "";
+		for (int i = 1; i <= 10; i++) {
+			nMultiplicar += "<h3>" + numero * i + "</h3>";
 		}
-		
+
 		return nMultiplicar;
 	}
+
+//Ejercicio 3.
+	
+	
+	
+	
 	
 	
 	
